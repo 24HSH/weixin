a00d1ec65b542f003a0e37c8ddb4c1b1b704009e
Jiakun Xu <jiakunxu@gmail.com>
2015-12-28 13:20:33 +0800

    tag

--- api/src/main/java/com/wideka/weixin/api/tag/ITagService.java
+++ api/src/main/java/com/wideka/weixin/api/tag/ITagService.java
@@ -1,6 +1,7 @@
 package com.wideka.weixin.api.tag;
 
 import com.wideka.weixin.api.tag.bo.Tag;
+import com.wideka.weixin.framework.bo.Result;
 
 /**
  * 
@@ -11,6 +12,11 @@ public interface ITagService {
 
 	String HTTPS_CREATE_URL = "https://qyapi.weixin.qq.com/cgi-bin/tag/create?access_token=";
 
+	String HTTPS_UPDATE_URL = "https://qyapi.weixin.qq.com/cgi-bin/tag/update?access_token=";
+
+	String HTTPS_DELETE_URL =
+		"https://qyapi.weixin.qq.com/cgi-bin/tag/delete?access_token=$ACCESS_TOKEN$&tagid=$TAGID$";
+
 	/**
 	 * 创建标签.
 	 * 
@@ -20,5 +26,24 @@ public interface ITagService {
 	 * @throws RuntimeException
 	 */
 	Tag createTag(String accessToken, Tag tag) throws RuntimeException;
+
+	/**
+	 * 更新标签名字.
+	 * 
+	 * @param accessToken
+	 * @param tag
+	 * @return
+	 * @throws RuntimeException
+	 */
+	Result updateTag(String accessToken, Tag tag) throws RuntimeException;
+
+	/**
+	 * 
+	 * @param accessToken
+	 * @param tagId
+	 * @return
+	 * @throws RuntimeException
+	 */
+	Result deleteTag(String accessToken, String tagId) throws RuntimeException;
 
 }
--- biz/src/main/java/com/wideka/weixin/tag/service/impl/TagServiceImpl.java
+++ biz/src/main/java/com/wideka/weixin/tag/service/impl/TagServiceImpl.java
@@ -8,6 +8,7 @@ import com.wideka.club.framework.util.HttpUtil;
 import com.wideka.club.framework.util.LogUtil;
 import com.wideka.weixin.api.tag.ITagService;
 import com.wideka.weixin.api.tag.bo.Tag;
+import com.wideka.weixin.framework.bo.Result;
 
 /**
  * 
@@ -36,6 +37,76 @@ public class TagServiceImpl implements ITagService {
 					HttpUtil.post(ITagService.HTTPS_CREATE_URL + accessToken.trim(), JSON.toJSONString(tag)), Tag.class);
 		} catch (Exception e) {
 			logger.error(LogUtil.parserBean(tag), e);
+
+			throw new RuntimeException("HttpUtil error.", e);
+		}
+
+		if (result == null) {
+			throw new RuntimeException("result is null.");
+		}
+
+		String errCode = result.getErrCode();
+		if (StringUtils.isNotBlank(errCode) && !"0".equals(errCode)) {
+			throw new RuntimeException(result.getErrMsg());
+		}
+
+		return result;
+	}
+
+	@Override
+	public Result updateTag(String accessToken, Tag tag) throws RuntimeException {
+		if (StringUtils.isBlank(accessToken)) {
+			throw new RuntimeException("access_token cannot be null.");
+		}
+
+		if (tag == null) {
+			throw new RuntimeException("tag cannot be null.");
+		}
+
+		Result result = new Result();
+
+		try {
+			result =
+				JSON.parseObject(
+					HttpUtil.post(ITagService.HTTPS_UPDATE_URL + accessToken.trim(), JSON.toJSONString(tag)),
+					Result.class);
+		} catch (Exception e) {
+			logger.error(LogUtil.parserBean(tag), e);
+
+			throw new RuntimeException("HttpUtil error.", e);
+		}
+
+		if (result == null) {
+			throw new RuntimeException("result is null.");
+		}
+
+		String errCode = result.getErrCode();
+		if (StringUtils.isNotBlank(errCode) && !"0".equals(errCode)) {
+			throw new RuntimeException(result.getErrMsg());
+		}
+
+		return result;
+	}
+
+	@Override
+	public Result deleteTag(String accessToken, String tagId) throws RuntimeException {
+		if (StringUtils.isBlank(accessToken)) {
+			throw new RuntimeException("access_token cannot be null.");
+		}
+
+		if (StringUtils.isBlank(tagId)) {
+			throw new RuntimeException("tagid cannot be null.");
+		}
+
+		Result result = new Result();
+
+		try {
+			result =
+				JSON.parseObject(
+					HttpUtil.get(ITagService.HTTPS_DELETE_URL.replace("$ACCESS_TOKEN$", accessToken.trim()).replace(
+						"$TAGID$", tagId.trim())), Result.class);
+		} catch (Exception e) {
+			logger.error(accessToken + "&" + tagId, e);
 
 			throw new RuntimeException("HttpUtil error.", e);
 		}
