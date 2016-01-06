5ade1f53e9e167d33af2d77b19081ebabdaf4381
Jiakun Xu <jiakunxu@gmail.com>
2016-01-06 15:32:52 +0800

    Media

--- api/src/main/java/com/wideka/weixin/api/media/IMediaService.java
+++ api/src/main/java/com/wideka/weixin/api/media/IMediaService.java
@@ -7,6 +7,10 @@ package com.wideka.weixin.api.media;
  */
 public interface IMediaService {
 
-	String HTTPS_UPLOAD_URL = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=$accessToken$&type=$type$";
+	String HTTPS_UPLOAD_URL =
+		"https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=$ACCESS_TOKEN$&type=$TYPE$";
+
+	String HTTPS_GET_URL =
+		"https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=$ACCESS_TOKEN$&media_id=$MEDIA_ID$";
 
 }
